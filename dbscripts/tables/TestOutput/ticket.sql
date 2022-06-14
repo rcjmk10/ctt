09f214d108ed4bbacd87f532270349cde52467af
tnkrf <kevinisbest3355@gmail.com>
2022-06-14 19:43:23 -0400

    testupdate

---
+++ dbscripts/tables/TestOutput/ticket.sql
@@ -0,0 +1,85 @@
+
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W4695-260', 'MAIN STREET NORTH','CDMA_Module10', '', 'Debug[COM][Sevr]: Invalid command received','5/10/2022 9:01', '', '5/10/2022 9:01', '0','5/10/2022 9:01', '43.9836', '-80.7382','POINT(43.9836 -80.7382)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W1163-2485', 'WOKING CRESCENT','CDMA_Module11', '', 'Debug[GEN][Crit]: VBat = 6124 mV, Reset = 0X4, Watchdog = 0X1','5/11/2022 9:01', '', '5/11/2022 9:01', '0','5/11/2022 9:01', '43.5251', '-79.6676','POINT(43.5251 -79.6676)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'C2437-1905', 'PILGRIMS WAY THE AR','GSM_Module11', '', '[GSM][Sevr]: Aborting oneshot reg','5/12/2022 9:01', '', '5/12/2022 9:01', '0','5/12/2022 9:01', '43.4437', '-79.7285','POINT(43.4437 -79.7285)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'C4573-1100', 'BURLOAK DRIVE OAKVI','GSM_Module12', '', '[GSM][Sevr]: Aborting oneshot data','5/13/2022 9:01', '', '5/13/2022 9:01', '0','5/13/2022 9:01', '43.3964', '-79.7597','POINT(43.3964 -79.7597)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'OTR0070-Toronto', 'ON 640 Guelph Line','GSM_Module13', '', '[GSM][Sevr]: Modem turn off failed','5/14/2022 9:01', '', '5/14/2022 9:01', '0','5/14/2022 9:01', '43.3433', '-79.7925','POINT(43.3433 -79.7925)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W5036-2015', 'PAN AM BLVD','GSM_Module14', '', '[GSM][Sevr]: Socket leak detected','5/15/2022 9:01', '', '5/15/2022 9:01', '0','5/15/2022 9:01', '43.4744', '-79.8766','POINT(43.4744 -79.8766)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'OTR0084', 'Oakville 2140Baronwood','GSM_Module15', '', '[GSM][Sevr]: Hard reset','5/16/2022 9:01', '', '5/16/2022 9:01', '0','5/16/2022 9:01', '43.4269', '-79.7631','POINT(43.4269 -79.7631)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W3071-5470', 'UPPER MIDDLE ROAD','ANTENA_GRP22', '', 'Debug[COM][Crit]: Unsupported BLE version 0001024E. Turning off.','5/17/2022 9:01', '', '5/17/2022 9:01', '0','5/17/2022 9:01', '43.4048', '-79.7768','POINT(43.4048 -79.7768)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'5500', 'North Service Rd','ANTENA_GRP23', '', 'Debug[ACC][Crit]: Accel error in State_Cfg','5/18/2022 9:01', '', '5/18/2022 9:01', '0','5/18/2022 9:01', '43.3961', '-79.7611','POINT(43.3961 -79.7611)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W1814-2535', 'REBECCA STREET','ANTENA_GRP24', '', '','5/19/2022 9:01', '', '5/19/2022 9:01', '0','5/19/2022 9:01', '43.3965', '-79.7222','POINT(43.3965 -79.7222)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W3693-115', 'COLE STREET','ANTENA_GRP25', '', 'Debug[GSM][Sevr]: Doing EMERG_OFF','5/20/2022 9:01', '', '5/20/2022 9:01', '0','5/20/2022 9:01', '43.3379', '-79.9091','POINT(43.3379 -79.9091)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W1667-1261', 'NORTH SHERIDAN WAY','ANTENA_GRP26', '', 'Debug[GSM][Sevr]: HTTP fail online-live2.services.u-blox.com:80 0/0','5/21/2022 9:01', '', '5/21/2022 9:01', '0','5/21/2022 9:01', '43.5413', '-79.6299','POINT(43.5413 -79.6299)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W5036-2015', 'PAN AM BLVD','GSM_Module15', '', 'Debug[GSM][Info]: APN xyz,,','5/22/2022 9:01', '', '5/22/2022 9:01', '0','5/22/2022 9:01', 'No Value', 'No Value','POINT(0 0)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'OTR0084', 'Oakville 2140Baronwood','GSM_Module16', '', 'Debug[GSM][Info]: Exhausted APNs xyz,,','5/23/2022 9:01', '', '5/23/2022 9:01', '0','5/23/2022 9:01', 'No Value', 'No Value','POINT(0 0)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W3071-5470', 'UPPER MIDDLE ROAD','GSM_Module17', '', 'Debug[GSM][Sevr]: HTTP fail online-live1.services.u-blox.com:80 0/4','5/24/2022 9:01', '', '5/24/2022 9:01', '0','5/24/2022 9:01', '43.4048', '-79.7768','POINT(43.4048 -79.7768)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'5500', 'North Service Rd','RF_A10', '', 'Debug[ACC][Crit]: Tumbler triggered cos^2<0','5/25/2022 9:01', '', '5/25/2022 9:01', '0','5/25/2022 9:01', '43.3961', '-79.7611','POINT(43.3961 -79.7611)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W1814-2535', 'REBECCA STREET','ANTENA_GRP28', '', 'Timeout Waiting for CTS','5/26/2022 9:01', '', '5/26/2022 9:01', '0','5/26/2022 9:01', '43.3965', '-79.7222','POINT(43.3965 -79.7222)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W3693-115', 'COLE STREET','ANTENA_GRP29', '', 'Debug[GSM][Sevr]: Hard Error: `AT` (0/3), LastRx: ``','5/27/2022 9:01', '', '5/27/2022 9:01', '0','5/27/2022 9:01', '43.3379', '-79.9091','POINT(43.3379 -79.9091)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'W1667-1261', 'NORTH SHERIDAN WAY','ANTENA_GRP30', '', '','5/28/2022 9:01', '', '5/28/2022 9:01', '0','5/28/2022 9:01', '43.5413', '-79.6299','POINT(43.5413 -79.6299)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'C2437-1905', 'PILGRIMS WAY THE AR','ANTENA_GRP31', '', '','5/29/2022 9:01', '', '5/29/2022 9:01', '0','5/29/2022 9:01', '43.4437', '-79.7285','POINT(43.4437 -79.7285)' );
+
+INSERT INTO public.ctt_tickets(
+ticket_id, "TowerID","TowerStreet", "ModuleID", "ErrorCode", "ErrorDetails", "ErrorDateTime","AssignedUser_ID", "AssignedDateTime", "Ticket_Status", "CompletedDateTime","Longitude", "Latitude", geom)
+VALUES (gen_random_uuid(),'C4573-1100', 'BURLOAK DRIVE OAKVI','RF_A14', '', 'Debug[GEN][Sevr]: JAMW','5/30/2022 9:01', '', '5/30/2022 9:01', '0','5/30/2022 9:01', '43.3964', '-79.7597','POINT(43.3964 -79.7597)' );
