8895888a6de9ea703363d0e90774f1064d600a6e
tnkrf <kevinisbest3355@gmail.com>
2022-07-04 00:50:53 -0400

    Server.py added addticket link for postman

--- WebApp/server.py
+++ WebApp/server.py
@@ -1,4 +1,5 @@
 from collections import UserDict, UserList
+import json
 from operator import methodcaller
 from flask import Flask, render_template, request, redirect, \
 url_for, flash, make_response, session, jsonify
@@ -7,6 +8,32 @@ import psycopg2
 
 app = Flask(__name__)
 app.secret_key = "any random string"
+
+def space_delimiter(Str, Select):
+    '''
+    returns either the portion of the string before the first " " in the string or everything after
+    depending on Selector
+
+    space_delimiter: Str, Str => Str
+    '''
+    str1 = ""
+    str2 = ""
+    bool = False
+    for letter in Str:
+        if bool == True:
+            str2 = str2+letter
+        elif letter == " ":
+            bool = True
+        else:
+            str1 = str1+letter
+    if Select == "Back":
+        return str2
+    elif Select == "Front":
+        return str1
+    else:
+        return "Invalid Select in space_delimiter in ticket_parse.py"
+
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -76,6 +103,58 @@ def logout():
    session.pop('password', None)
    session.pop('authenticationtoken', False)
    return redirect(url_for('home'))
+
+@app.route('/tickets/addnewticket', methods = ['POST'])
+def process_json():
+
+    conn = None
+
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        template = 'INSERT INTO public.ctt_tickets(\nticket_id, \"TowerID\",\
+\"TowerStreet\", \"ModuleID\", \"ErrorCode\", \"ErrorDetails\", \"ErrorDateTime\",\
+\"AssignedUser_ID\", \"AssignedDateTime\", \"Ticket_Status\", \"CompletedDateTime\",\
+\"Longitude\", \"Latitude\", geom)\nVALUES (gen_random_uuid(),\
+\'{0}\', \'{1}\',\
+\'{2}\', \'\', \'{3}\',\
+\'{4}\', \'\', \'{4}\', \'0\',\
+\'{4}\', \'{5}\', \'{6}\',\
+\'POINT'+"({5} {6})"+'\' );'
+
+        ticket = request.json
+        insertcommand = ""
+        towerid = space_delimiter(ticket["Tower"], "Front")
+        towerstreet = space_delimiter(ticket["Tower"], "Back")
+        moduleid = ticket["Equipment"]
+        #errorcode = ""
+        errordetails = ticket["Message"]
+        errordatetime = ticket["RegDate"]
+        #assigneduserid = ""
+        #assigneddatetime = ticket["RegDate"]
+        #ticketstatus = "0"
+        #completeddatetime = ticket["RegDate"]
+        long = ticket["TowerLongitude"]
+        lat = ticket["TowerLatitude"]
+
+        insertcommand = template.format(towerid, towerstreet, moduleid, errordetails, errordatetime, long, lat)
+        print(insertcommand)
+
+        try:
+            conn = psycopg2.connect( host="localhost", database="ctt", user="postgres", password="cynthus2003")
+            cur = conn.cursor()
+
+            #Statement Execution
+            cur.execute(insertcommand)
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
+            return "completed"
+    else:
+        return 'Content-Type not supported!'
 
 
 
